2efa4b79a3ba1fd0e28b3981614cc1d70aafb761
Anatoliia Riabchenko <108002173+AnatoliiaR@users.noreply.github.com>
2022-12-05 12:51:51 +0200

    11

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRefs = document.querySelector('.gallery');
+
+function createGalerry(items) {
+    return items.map(({ preview, original, description }) => 
+        
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    ).join('');
+};
+
+const galleryMarkup = createGalerry(galleryItems);
+galleryRefs.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryRefs.addEventListener('click', onItemClick);
+
+
+function onItemClick(e) {
+
+    e.preventDefault();
+
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const gallery = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        docClose: true,
+
+
+    });
+  
+
+}
