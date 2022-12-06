4d30581f40bd5dc99dd2a5f13f0b4e634eaed239
Anatoliia Riabchenko <108002173+AnatoliiaR@users.noreply.github.com>
2022-12-06 15:57:47 +0200

    55
    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,24 @@
 import throttle from 'lodash.throttle'; 
+import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+
+
+// ===================
+// player.on('timeupdate', throttle(setWatchingTime, 1000));
+
+
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay({ seconds }) {
-    localStorage.setItem('videoplayer-current-time', seconds);
+
+function onPlay(e) {
+
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+
 }
+
+
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
