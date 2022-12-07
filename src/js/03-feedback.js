27fbf264028b4906a9c9d73297dfe4ac32e97b41
Anatoliia Riabchenko <108002173+AnatoliiaR@users.noreply.github.com>
2022-12-07 09:43:39 +0200

    66

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,37 @@
 import throttle from 'lodash.throttle'; 
 
 const STORAGE_KEY = 'feedback-form-state';
-
-const FormData = {};
-
-
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  msg: document.querySelector('.feedback-form textarea'),
-  input: document.querySelector('input'),
-};
-
 const form = document.querySelector('.feedback-form');
-refs.form.addEventListener('input', throttle(onFormInput, 500));
-refs.form.addEventListener('submit', onFormSub);
+const getFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+// const LS = localStorage;
 
-updateForm();
+
+
+if (getFormData) {
+  form.elements.email.value = getFormData.email;
+  form.elements.message.value = getFormData.message;
+}
+
+function onFormInput() {
+     const FormData = {};
+    FormData.email = form.email.value;
+    FormData.message = form.message.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(FormData));
+
+}
+
+// updateForm();
 
 function onFormSub(e) {
     e.preventDefault();
-
+console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+ 
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+
+
 }
 
-
-function onFormInput(e) {
-    FormData[e.target.name] = e.target.value;
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(FormData));
-}
-
-
-function updateForm() {
-    const saveMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-    if (saveMessage === null) {
-       
-        return;
-    }
-
-  refs.msg.value = saveMessage['message'] || '';
-  refs.input.value = saveMessage['email'] || '';
-
-}
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSub);
